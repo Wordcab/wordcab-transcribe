ae1fade95640ec2c6db41f74f62bd3cb3eb21735
chainyo <thomas@wordcab.com>
2023-03-30 08:00:40 +0000

    fix contact email

--- asr_api/main.py
+++ asr_api/main.py
@@ -45,7 +45,7 @@ async def health_check():
             <h1 class="text-4xl font-medium">{settings.project_name}</h1>
             <p class="text-gray-600">Version: {settings.version}</p>
             <p class="text-gray-600">{settings.description}</p>
-            <p class="mt-16 text-gray-500">Want access? Contact us: <a class="text-blue-400 text-underlined" href="mailto:contact@deeptools.ai?subject=Access">contact@deeptools.ai</a></p>
+            <p class="mt-16 text-gray-500">Want access? Contact us: <a class="text-blue-400 text-underlined" href="mailto:info@wordcab.com?subject=Access">info@wordcab.com</a></p>
             <a href="/docs">
                 <button class="mt-8 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Docs</button>
             </a>
