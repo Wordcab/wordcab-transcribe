76c31d712b706fedfc91d59a44426518968e1adf
chainyo <thomas@wordcab.com>
2023-06-12 14:05:15 +0000

    fix cortex_response for SVIX size limit

--- wordcab_transcribe/router/v1/cortex_endpoint.py
+++ wordcab_transcribe/router/v1/cortex_endpoint.py
@@ -35,6 +35,7 @@ from wordcab_transcribe.models import (
 )
 from wordcab_transcribe.router.v1.audio_url_endpoint import inference_with_audio_url
 from wordcab_transcribe.router.v1.youtube_endpoint import inference_with_youtube
+from wordcab_transcribe.utils import remove_words_for_svix
 
 
 router = APIRouter()
@@ -108,7 +109,9 @@ async def run_cortex(
         "request_id": request_id,
     }
 
-    await send_update_with_svix(payload.job_name, "finished", _cortex_response)
+    await send_update_with_svix(
+        payload.job_name, "finished", remove_words_for_svix(_cortex_response)
+    )
 
     if payload.url_type == "youtube":
         return CortexYoutubeResponse(**_cortex_response)
