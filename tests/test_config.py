5abfc82061da5811e7fc51a300a9fcdf27dc35e6
chainyo <thomas@wordcab.com>
2023-06-21 10:13:31 +0000

    fix tests

--- tests/test_config.py
+++ tests/test_config.py
@@ -72,7 +72,7 @@ def test_config() -> None:
     assert settings.whisper_model == "large-v2"
     assert settings.compute_type == "float16"
     assert settings.extra_languages == ["he"]
-    assert settings.extra_languages_model_paths == {"he": "path/to/model"}
+    assert settings.extra_languages_model_paths == {"he": ""}
 
     assert settings.nemo_domain_type == "telephonic"
     assert settings.nemo_storage_path == "nemo_storage"
