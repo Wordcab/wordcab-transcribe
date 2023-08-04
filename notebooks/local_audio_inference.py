6a842b1b8f18a5eabc54286c57bca98a3d35ebf5
Thomas Chaigneau <thomas@wordcab.com>
2023-08-04 14:33:47 +0200

    Update API models (#188)
    * update api models
    
    * exclude notebooks folder to quality tests

--- notebooks/local_audio_inference.py
+++ notebooks/local_audio_inference.py
@@ -5,12 +5,12 @@ import requests
 filepath = "data/short_one_speaker.mp3"
 
 data = {
-  "alignment": False,  # Longer processing time but better timestamps
-  "diarization": True,  # Longer processing time but speaker segment attribution
-  "dual_channel": False,  # Only for stereo audio files with one speaker per channel
-  "source_lang": "ru",  # optional, default is "en"
-  "timestamps": "s",  # optional, default is "s". Can be "s", "ms" or "hms".
-  "word_timestamps": False,  # optional, default is False
+    "alignment": False,  # Longer processing time but better timestamps
+    "diarization": True,  # Longer processing time but speaker segment attribution
+    "dual_channel": False,  # Only for stereo audio files with one speaker per channel
+    "source_lang": "ru",  # optional, default is "en"
+    "timestamps": "s",  # optional, default is "s". Can be "s", "ms" or "hms".
+    "word_timestamps": False,  # optional, default is False
 }
 
 with open(filepath, "rb") as f:
@@ -25,4 +25,4 @@ r_json = response.json()
 
 filename = filepath.split(".")[0]
 with open(f"{filename}.json", "w", encoding="utf-8") as f:
-  json.dump(r_json, f, indent=4, ensure_ascii=False)
+    json.dump(r_json, f, indent=4, ensure_ascii=False)
