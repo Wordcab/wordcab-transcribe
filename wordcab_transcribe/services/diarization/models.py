08620441d46e03831b66f1c1ab54251963fa64d2
chainyo <thomas@wordcab.com>
2023-08-11 08:25:58 +0000

    add encoder

--- wordcab_transcribe/services/diarization/models.py
+++ wordcab_transcribe/services/diarization/models.py
@@ -27,13 +27,30 @@ import tarfile
 import wget
 import yaml
 from pathlib import Path
-from typing import List, NamedTuple, Tuple
+from typing import List, NamedTuple, Optional, Tuple
 
 import librosa
 import torch
 import torch.nn as nn
 
 from wordcab_transcribe.services.diarization.utils import resolve_diarization_cache_dir
+from wordcab_transcribe.services.diarization.modules import (
+    JasperBlock,
+    MaskedConv1d,
+    SqueezeExcite,
+    init_weights,
+)
+
+
+ACTIVATION_REGISTRY = {
+    "identity": nn.Identity,
+    "hardtanh": nn.Hardtanh,
+    "relu": nn.ReLU,
+    "selu": nn.SELU,
+    "swish": nn.SiLU,
+    "silu": nn.SiLU,
+    "gelu": nn.GELU,
+}
 
 
 # https://github.com/NVIDIA/NeMo/blob/main/nemo/collections/asr/parts/preprocessing/features.py#L59
@@ -103,6 +120,153 @@ class MultiscaleEmbeddingsAndTimestamps(NamedTuple):
     embeddings: List[torch.Tensor]
     timestamps: List[torch.Tensor]
     multiscale_weights: List[float]
+
+
+# https://github.com/NVIDIA/NeMo/blob/main/nemo/collections/asr/modules/conv_asr.py#L56
+class ConvASREncoder:
+    """
+    Convolutional encoder for ASR models. With this class you can implement JasperNet and QuartzNet models.
+
+    Based on these papers:
+        https://arxiv.org/pdf/1904.03288.pdf
+        https://arxiv.org/pdf/1910.10261.pdf
+    """
+
+    def input_example(self, max_batch=1, max_dim=8192):
+        """
+        Generates input examples for tracing etc.
+        Returns:
+            A tuple of input examples.
+        """
+        device = next(self.parameters()).device
+        input_example = torch.randn(max_batch, self._feat_in, max_dim, device=device)
+        lens = torch.full(size=(input_example.shape[0],), fill_value=max_dim, device=device)
+        return tuple([input_example, lens])
+
+    def __init__(
+        self,
+        jasper: dict,
+        activation: str,
+        feat_in: int,
+        normalization_mode: str = "batch",
+        residual_mode: str = "add",
+        norm_groups: int = -1,
+        conv_mask: bool = True,
+        frame_splicing: int = 1,
+        init_mode: Optional[str] = "xavier_uniform",
+        quantize: bool = False,
+    ):
+        super().__init__()
+
+        activation = ACTIVATION_REGISTRY[activation]()
+
+        # If the activation can be executed in place, do so.
+        if hasattr(activation, "inplace"):
+            activation.inplace = True
+
+        feat_in = feat_in * frame_splicing
+
+        self._feat_in = feat_in
+
+        residual_panes = []
+        encoder_layers = []
+        self.dense_residual = False
+        for layer_idx, lcfg in enumerate(jasper):
+            dense_res = []
+            if lcfg.get("residual_dense", False):
+                residual_panes.append(feat_in)
+                dense_res = residual_panes
+                self.dense_residual = True
+
+            groups = lcfg.get('groups', 1)
+            separable = lcfg.get('separable', False)
+            heads = lcfg.get('heads', -1)
+            residual_mode = lcfg.get('residual_mode', residual_mode)
+            se = lcfg.get('se', False)
+            se_reduction_ratio = lcfg.get('se_reduction_ratio', 8)
+            se_context_window = lcfg.get('se_context_size', -1)
+            se_interpolation_mode = lcfg.get('se_interpolation_mode', 'nearest')
+            kernel_size_factor = lcfg.get('kernel_size_factor', 1.0)
+            stride_last = lcfg.get('stride_last', False)
+            future_context = lcfg.get('future_context', -1)
+
+            encoder_layers.append(
+                JasperBlock(
+                    feat_in,
+                    lcfg['filters'],
+                    repeat=lcfg['repeat'],
+                    kernel_size=lcfg['kernel'],
+                    stride=lcfg['stride'],
+                    dilation=lcfg['dilation'],
+                    dropout=lcfg['dropout'],
+                    residual=lcfg['residual'],
+                    groups=groups,
+                    separable=separable,
+                    heads=heads,
+                    residual_mode=residual_mode,
+                    normalization=normalization_mode,
+                    norm_groups=norm_groups,
+                    activation=activation,
+                    residual_panes=dense_res,
+                    conv_mask=conv_mask,
+                    se=se,
+                    se_reduction_ratio=se_reduction_ratio,
+                    se_context_window=se_context_window,
+                    se_interpolation_mode=se_interpolation_mode,
+                    kernel_size_factor=kernel_size_factor,
+                    stride_last=stride_last,
+                    future_context=future_context,
+                    quantize=quantize,
+                    layer_idx=layer_idx,
+                )
+            )
+            feat_in = lcfg['filters']
+
+        self._feat_out = feat_in
+
+        self.encoder = torch.nn.Sequential(*encoder_layers)
+        self.apply(lambda x: init_weights(x, mode=init_mode))
+
+        self.max_audio_length = 0
+
+    def forward(self, audio_signal, length):
+        self.update_max_sequence_length(seq_length=audio_signal.size(2), device=audio_signal.device)
+        s_input, length = self.encoder(([audio_signal], length))
+        if length is None:
+            return s_input[-1]
+
+        return s_input[-1], length
+
+    def update_max_sequence_length(self, seq_length: int, device):
+        # Find global max audio length across all nodes
+        if torch.distributed.is_initialized():
+            global_max_len = torch.tensor([seq_length], dtype=torch.float32, device=device)
+
+            # Update across all ranks in the distributed system
+            torch.distributed.all_reduce(global_max_len, op=torch.distributed.ReduceOp.MAX)
+
+            seq_length = global_max_len.int().item()
+
+        if seq_length > self.max_audio_length:
+            if seq_length < 5000:
+                seq_length = seq_length * 2
+            elif seq_length < 10000:
+                seq_length = seq_length * 1.5
+            self.max_audio_length = seq_length
+
+            device = next(self.parameters()).device
+            seq_range = torch.arange(0, self.max_audio_length, device=device)
+            if hasattr(self, 'seq_range'):
+                self.seq_range = seq_range
+            else:
+                self.register_buffer('seq_range', seq_range, persistent=False)
+
+            # Update all submodules
+            for _, m in self.named_modules():
+                if isinstance(m, MaskedConv1d):
+                    m.update_masked_length(self.max_audio_length, seq_range=self.seq_range)
+                elif isinstance(m, SqueezeExcite):
+                    m.set_max_len(self.max_audio_length, seq_range=self.seq_range)
 
 
 # This code is from NVIDIA NeMo toolkit package `FilterbankFeaturesTA` class:
@@ -343,7 +507,7 @@ class EncDecSpeakerLabelModel:
             model_config = yaml.safe_load(config_file)
 
         self.preprocessor = MelSpectrogramPreprocessor(**model_config["preprocessor"])
-        self.encoder = None
+        self.encoder = ConvASREncoder(**model_config["encoder"])
         self.decoder = None
 
     def forward() -> None:
