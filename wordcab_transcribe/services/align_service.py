744ec95675dcda61b805201f5cae005ca42d12f7
chainyo <thomas@wordcab.com>
2023-08-14 16:08:58 +0000

    add BSD 3-Clause to whisperx

--- wordcab_transcribe/services/align_service.py
+++ wordcab_transcribe/services/align_service.py
@@ -17,6 +17,26 @@
 #
 # See the License for the specific language governing permissions
 # and limitations under the License.
+
+# BSD 3-Clause License
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+# Alignment module is based on the paper
+# https://arxiv.org/abs/2303.00747.pdf and the implementation from
+# https://github.com/m-bain/whisperX
+
+# Code from this module is heavily based on the m-bain's WhisperX implementation:
+# https://github.com/m-bain/whisperX/blob/main/whisperx/alignment.py
 """Alignment Service for transcribed audio files."""
 
 from collections import OrderedDict
